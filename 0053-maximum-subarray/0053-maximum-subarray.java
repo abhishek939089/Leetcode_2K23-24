df96c03c2607fd6f715e464afe79dc37baa3c691
Abhishek Sharma <100137322+abhishek939089@users.noreply.github.com>
2024-01-04 21:13:56 +0530

    Time: 1 ms (99.98%), Space: 57.3 MB (78.20%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,19 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int rslt = nums[0];
-        int sum = nums[0];
- 
-        for(int i=1; i<nums.length; i++){
-            sum = Math.max(nums[i], sum + nums[i]);
-            rslt = Math.max(rslt, sum);
+//         Kadanes's algo
+        int sum = 0;
+        int maxi = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            if(sum > maxi) {
+                maxi = sum;
+            }
+            
+            if(sum < 0) {
+                sum = 0;
+            }
         }
-    return rslt;
+        return maxi;
+        
     }
 }
