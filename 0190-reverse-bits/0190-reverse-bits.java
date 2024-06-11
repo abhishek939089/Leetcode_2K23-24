aaea0d102950b2e1214f818790aab84e0f5714f2
Abhishek Sharma <100137322+abhishek939089@users.noreply.github.com>
2024-06-11 10:41:15 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (64.50%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,10 +1,10 @@
 public class Solution {
     public int reverseBits(int n) {
         int count = 0;
-        for (int i = 0; i < 32; i++) {
-            count <<= 1; 
-            count = count | (n & 1); 
-            n >>= 1;
+        for(int i=0; i<32; i++) {
+            count<<=1;
+            count = count | (n&1);
+            n>>=1;
         }
         return count;
     }
