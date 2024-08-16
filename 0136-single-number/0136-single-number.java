b7e2b197482bad43d19dd44b6737cb5b6ee26fe2
Abhishek Sharma <100137322+abhishek939089@users.noreply.github.com>
2024-08-16 22:52:55 +0530

    Time: 12 ms (23.22%), Space: 45 MB (88.29%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,15 +1,31 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int len = nums.length;
-        int i = 0;
-        while(i < len - 1) {
-            if(nums[i] == nums[i+1]) {
-                i = i+2;
-            }else {
-                return nums[i];
+        // Arrays.sort(nums);
+        // int len = nums.length;
+        // int i = 0;
+        // while(i < len - 1) {
+        //     if(nums[i] == nums[i+1]) {
+        //         i = i+2;
+        //     }else {
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[len - 1]; //by default it will return last element of sorted array id it does't find any non-duplicate number
+        // int ans = 0;
+        // for(int number : nums) {
+        //     ans ^= number;
+        // }
+        // return ans;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for(int num : nums) {
+            if(map.get(num) == 1) {
+                return num;
             }
         }
-        return nums[i];
+        return -1;
     }
 }
